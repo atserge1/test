2bd7a6c8f88aeb75a3e365fd499812f4317a16b7
atserge1 <vranceans@gmail.com>
2023-01-31 15:13:26 -0800

    aa

--- Class-1/Example-1/instance.tf
+++ Class-1/Example-1/instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "myinstance" {
-  ami           = "ami-0b5eea76982371e91"
+           = "ami-0b5eea76982371e91"
   instance_type = "t2.micro"
 
   key_name        = "bastion-key"
